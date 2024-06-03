c92d8a7e527ebf1cfc28afe2f02c568b1a42a62d
kaito <kaito1013kaito@gmail.com>
2024-06-03 15:28:26 +0900

    edit header

--- src/components/layouts/Header/Header.tsx
+++ src/components/layouts/Header/Header.tsx
@@ -28,71 +28,73 @@ const Header = () => {
     ];
 
     return (
-        <Navbar isBordered isMenuOpen={isMenuOpen} onMenuOpenChange={setIsMenuOpen}>
-            <NavbarContent justify="start" className="sm:hidden">
-                <NavbarMenuToggle
-                    aria-label={isMenuOpen ? "Close menu" : "Open menu"}
-                />
-            </NavbarContent>
-            
+        <header>
+            <Navbar isBordered isMenuOpen={isMenuOpen} onMenuOpenChange={setIsMenuOpen}>
+                <NavbarContent justify="start" className="sm:hidden">
+                    <NavbarMenuToggle
+                        aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+                    />
+                </NavbarContent>
+                
 
-            <NavbarContent className="hidden sm:flex gap-5">
-                <NavbarItem>
-                    <Link href="/" color="foreground">
-                        Home
-                    </Link>
-                </NavbarItem>
-                <NavbarItem>
-                    <Link href="/profile" color="foreground">
-                        Profile
-                    </Link>
-                </NavbarItem>
-                <NavbarItem>
-                    <Link href="/works" color="foreground">
-                        Works
-                    </Link>
-                </NavbarItem>
-                <NavbarItem>
-                    <Link href="/contact" color="foreground">
-                        Contact
-                    </Link>
-                </NavbarItem>
-                <NavbarItem>
-                    <Links />
-                </NavbarItem>
-            </NavbarContent>
-
-            <NavbarContent justify="end" className="flex">
-                <NavbarItem>
-                    <Link href="https://github.com/Katsudon10" color="foreground" isExternal>
-                        <SiGithub size={20} color="foreground"/>
-                    </Link>
-                </NavbarItem>
-                <NavbarItem>
-                    <Link href="https://www.linkedin.com/in/kaitoinoue/" color="foreground" isExternal>
-                        <SiLinkedin size={20} color="foreground"/>
-                    </Link>
-                </NavbarItem>
-                <NavbarItem>
-                    <Link href="https://www.wantedly.com/id/Kaito_Inoue10" color="foreground" isExternal>
-                        <SiWantedly size={20} color="foreground"/>
-                    </Link>
-                </NavbarItem>
-            </NavbarContent>
-
-            <NavbarMenu>
-                {menuItems.map((item, index) => (
-                    <NavbarMenuItem key={index}>
-                        <Link color="foreground" href={item.path}>
-                            {item.name}
+                <NavbarContent className="hidden sm:flex gap-5">
+                    <NavbarItem>
+                        <Link href="/" color="foreground">
+                            Home
                         </Link>
+                    </NavbarItem>
+                    <NavbarItem>
+                        <Link href="/profile" color="foreground">
+                            Profile
+                        </Link>
+                    </NavbarItem>
+                    <NavbarItem>
+                        <Link href="/works" color="foreground">
+                            Works
+                        </Link>
+                    </NavbarItem>
+                    <NavbarItem>
+                        <Link href="/contact" color="foreground">
+                            Contact
+                        </Link>
+                    </NavbarItem>
+                    <NavbarItem>
+                        <Links />
+                    </NavbarItem>
+                </NavbarContent>
+
+                <NavbarContent justify="end" className="flex">
+                    <NavbarItem>
+                        <Link href="https://github.com/Katsudon10" color="foreground" isExternal>
+                            <SiGithub size={20} color="foreground"/>
+                        </Link>
+                    </NavbarItem>
+                    <NavbarItem>
+                        <Link href="https://www.linkedin.com/in/kaitoinoue/" color="foreground" isExternal>
+                            <SiLinkedin size={20} color="foreground"/>
+                        </Link>
+                    </NavbarItem>
+                    <NavbarItem>
+                        <Link href="https://www.wantedly.com/id/Kaito_Inoue10" color="foreground" isExternal>
+                            <SiWantedly size={20} color="foreground"/>
+                        </Link>
+                    </NavbarItem>
+                </NavbarContent>
+
+                <NavbarMenu>
+                    {menuItems.map((item, index) => (
+                        <NavbarMenuItem key={index}>
+                            <Link color="foreground" href={item.path}>
+                                {item.name}
+                            </Link>
+                        </NavbarMenuItem>
+                    ))}
+                    <NavbarMenuItem>
+                        <Links />
                     </NavbarMenuItem>
-                ))}
-                <NavbarMenuItem>
-                    <Links />
-                </NavbarMenuItem>
-            </NavbarMenu>
-        </Navbar>
+                </NavbarMenu>
+            </Navbar>
+        </header>
     );
 }
 
