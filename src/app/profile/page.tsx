9a5e64c5a186b7ae55162a3b9b48d3bd884dbe80
kaito <kaito1013kaito@gmail.com>
2024-06-27 03:28:12 +0900

    Add Profile component to profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,9 +1,8 @@
+import Profile from "@/components/layouts/Profile/Profile";
+
 const Page = () => {
     return (
-        <div>
-        <h1>Profile</h1>
-        <p>This is the profile page</p>
-        </div>
+        <Profile />
     );
 }
 
