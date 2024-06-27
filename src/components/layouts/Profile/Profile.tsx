ea38e8b264a2ba017f90bfe713c3b9f10ffc5d25
kaito <kaito1013kaito@gmail.com>
2024-06-28 03:18:14 +0900

    Update Profile layout to display skills in a grid

--- src/components/layouts/Profile/Profile.tsx
+++ src/components/layouts/Profile/Profile.tsx
@@ -5,7 +5,7 @@ import Tool from "@/components/layouts/Profile/Tool";
 
 const Profile = () => {
     return (
-        <div className="container mx-auto">
+        <div className="justify-center container mx-auto grid grid-cols-1 justify-items-center gap-4 md:grid-cols-2 ">
             <Frontend />
             <Backend />
             <Infrastructure />
